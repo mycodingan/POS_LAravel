7634e48afbc81f117a27444a41c3dff91953c1c1
joshua <joshuapplg@gmail.com>
2024-03-19 15:33:10 +0700

    dev#1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,20 +20,17 @@
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-ligth">
 
   <div class="container">
-
-    <!-- Outer Row -->
     <div class="row justify-content-center">
 
       <div class="col-xl-5 col-lg-12 col-md-9">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
             <div class="row">
-             
+
               <div class="col-lg-12">
                 <div class="p-5">
                   <div class="text-center">
@@ -57,11 +54,11 @@
                         <strong>{{ $message }}</strong
                     @enderror
 
-                    
+
                     <button type="submit" class="btn btn-primary btn-user btn-block">
                       {{ __('Login') }}
                     </button>
-                    
+
                   </form>
                   <hr>
                   <!-- <div class="text-center">
@@ -82,14 +79,9 @@
 
   </div>
 
-  <!-- Bootstrap core JavaScript-->
   <script src="{{url('assets/vendor/jquery/jquery.min.js')}}"></script>
   <script src="{{url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-  <!-- Core plugin JavaScript-->
   <script src="{{url('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-  <!-- Custom scripts for all pages-->
   <script src="{{url('assets/js/sb-admin-2.min.js')}}"></script>
 
 </body>
